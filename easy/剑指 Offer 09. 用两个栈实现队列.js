841eb6046408c7f84b781d57778391061c4067cb
折莹 <sheying@meituan.com>
2021-07-05 21:42:33 +0800

    feat: 剑指offer 09：使用两个栈实现队列

--- easy/剑指 Offer 09. 用两个栈实现队列.js
+++ easy/剑指 Offer 09. 用两个栈实现队列.js
@@ -4,7 +4,8 @@
  */
 
 const CQueue = function () {
-
+  this.stackA = []
+  this.stackB = []
 };
 
 /** 
@@ -12,14 +13,25 @@ const CQueue = function () {
  * @return {void}
  */
 CQueue.prototype.appendTail = function (value) {
-
+  this.stackA.push(value)
 };
 
 /**
  * @return {number}
  */
 CQueue.prototype.deleteHead = function () {
-
+  if (this.stackB.length) {
+    return this.stackB.pop()
+  } else {
+    while (this.stackA.length) {
+      this.stackB.push(this.stackA.pop())
+    }
+    if (!this.stackB.length) {
+      return -1
+    } else {
+      return this.stackB.pop()
+    }
+  }
 };
 
 /**
